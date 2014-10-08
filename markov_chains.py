8781fc13ee164fc9eaf569047604e229e56b963a
Mars Williams <metaphor.formation@gmail.com>
2014-10-08 02:12:27 -0700

    Update markov_chains.py
    takes one file as input, creates a markov chain dictionary, and returns a new, pseudo-randomized output from the dictionary. still need to determine Type Error raised when passing dict created from set union, in order to pass two files for mashup.

--- markov_chains.py
+++ markov_chains.py
@@ -1,65 +1,117 @@
 #!/usr/bin/env python
-# songs_lyrics1=open("Bonobo.txt", "r")
-# songs_lyrics2=open("Ellery.txt", "r")
 
 from sys import argv
-from string import punctuation
-from string import digits
+
+from collections import Counter
+
+import random
 
 
-def make_text(chains):
-    """Takes a dictionary of markov chains and returns random text
-    based off an original text."""
-    return "Here's some random text."
+# def make_text(merged_word_list):
+#     """Takes a dictionary of markov chains and returns random text
+#     based off an original text."""
+
+#     max_words = 40
+
+#     first_words = random.choice(merged_word_list.keys())
+
+#     for i in range(max_words):
+
+#         next_words = merged_word_list.get(first_words, None)
+
+#         if next_words == None:
+#             # if the start isn't in map, we got to the end of the
+#             # original text, so we have to start again.
+#             make_text(max_words-i)
+#             return
+
+#         # choose a random suffix
+#         word = random.choice(next_words)
+
+#         print word,
+
+#         first_words = new_words(first_words, word)
+
+#     print "."
+
+def make_text(word_list):
+
+    max_words = 40
+
+    first_words = random.choice(word_list.keys())
+
+    for i in range(max_words):
+        next_words = word_list.get(first_words, None)
+
+        if next_words == None:
+            make_text(max_words-i)
+            return
+
+        word = random.choice(next_words)
+
+        if i == 0:
+            print word.capitalize(),
+        elif i == max_words-1:
+            print word + "."
+        else:
+            print word,
+
+        first_words = new_words(first_words, word)
+
+#want to modify function to check for period after value, to capitalize following value
+
+
+def new_words(first_words, word):
+
+    return first_words[1:] + (word,)
+
+
+    #with merged_word_list error is TypeError: cannot concatenate 'str' and 'tuple' objects
+
 
 
 def make_chains(filename):
     word_list={}
-    with open(filename) as song_lyrics_text:
-        txt = song_lyrics_text.read()
-        words = txt.strip().lower().split()
-    # print words
 
-            #removes nonalpha characters
-            # for char in nonwords:
-            #     aline = aline.replace(char, "")
+    with open(filename) as text_sample:
+        text_sample = text_sample.read()
+        words = text_sample.strip().lower().split()
 
-            #returns list of words
-            # words = aline.split() 
         
-        for i in range(len(words)-2):
-            key = (words[i], words[i+1])
-            value = words[i+2]
+        for i in range(len(words)-3):
+            key = (words[i], words[i+1], words[i+2])
+            value = words[i+3]
             word_list[key] = word_list.get(key, [value])
-            # if key not in word_list.keys():
-            #     word_list[key] = [value]
-            # else:
-            #     word_list[key].append(value)
 
     return word_list
 
 
-    #print word_list
+# def link_chains(word_list_1, word_list_2):
+
+#     word_list_1, word_list_2 = set(word_list_1), set(word_list_2)
+
+#     merged_word_list = dict(word_list_1 | word_list_2)
+
+#     # print type(merged_word_list) prints <type 'dict'>
 
 
-    # with open(song_lyric_choices[2]) as song_lyrics_second:
 
-    # chain_dict = make_chains(input_text)
-    # random_text = make_text(chain_dict)
-    # print random_text
+# def main(filename_1, filename_2):
 
-def main(song_lyrics1, songs_lyrics2):
-    song_1 = make_chains(song_lyrics1)
-    song_2 = make_chains(songs_lyrics2)
+#     word_list_1, word_list_2 =  make_chains(filename_1), make_chains(filename_2)
+
+#     merged_word_list = link_chains(word_list_1, word_list_2)
+
+#     # new_text = make_text(merged_word_list)
+
+def main(filename):
+    word_list = make_chains(filename)
+
+    output_text = make_text(word_list) 
 
 
-    make_text(song_1)
-    make_text(song_2)
     
-    # Change this to read input_text from a file
-    # nonwords = punctuation + digits
-
-    #reads file
 
 if __name__ == "__main__":
-    main(argv[1], argv[2])
+    # main(argv[1], argv[2])
+    main(argv[1])
