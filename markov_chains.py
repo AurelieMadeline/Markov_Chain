9bc6771487f63d9d85a335b47f99f1c8c99ec173
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 17:50:49 -0700

    created two seperate markov chain dictionaries, but haven't mixed them together yet.

--- markov_chains.py
+++ markov_chains.py
@@ -6,45 +6,60 @@ from sys import argv
 from string import punctuation
 from string import digits
 
-def make_chains(corpus):
-    """Takes an input text as a string and returns a dictionary of
-    markov chains."""
-
-    #populates dictionary of words and frequency for song_lyrics_first
-
-    return {}
 
 def make_text(chains):
     """Takes a dictionary of markov chains and returns random text
     based off an original text."""
     return "Here's some random text."
 
-def main(song_lyric_choices):
-    word_list={}
-    # Change this to read input_text from a file
-    nonwords = punctuation + digits
 
-    #reads file
-    with open(songs_lyric_choices[1]) as song_lyrics_first:
-        for line in song_lyrics_first:
-            aline = line.strip().lower()
+def make_chains(filename):
+    word_list={}
+    with open(filename) as song_lyrics_text:
+        txt = song_lyrics_text.read()
+        words = txt.strip().lower().split()
+    # print words
 
             #removes nonalpha characters
-            for char in nonwords:
-                aline = aline.replace(char, "")
+            # for char in nonwords:
+            #     aline = aline.replace(char, "")
 
             #returns list of words
-            aline = aline.split()
+            # words = aline.split() 
+        
+        for i in range(len(words)-2):
+            key = (words[i], words[i+1])
+            value = words[i+2]
+            word_list[key] = word_list.get(key, [value])
+            # if key not in word_list.keys():
+            #     word_list[key] = [value]
+            # else:
+            #     word_list[key].append(value)
 
+    return word_list
+
+
+    #print word_list
+
+
+    # with open(song_lyric_choices[2]) as song_lyrics_second:
+
+    # chain_dict = make_chains(input_text)
+    # random_text = make_text(chain_dict)
+    # print random_text
+
+def main(song_lyrics1, songs_lyrics2):
+    song_1 = make_chains(song_lyrics1)
+    song_2 = make_chains(songs_lyrics2)
+
+
+    make_text(song_1)
+    make_text(song_2)
     
+    # Change this to read input_text from a file
+    # nonwords = punctuation + digits
 
-
-
-    with open(song_lyric_choices[2]) as song_lyrics_second:
-
-    chain_dict = make_chains(input_text)
-    random_text = make_text(chain_dict)
-    print random_text
+    #reads file
 
 if __name__ == "__main__":
     main(argv[1], argv[2])
